5a8bc65ed7016a67145c5bce85de2737973a9bfe
Loyanne Moreira <52591515+loyanne-santos@users.noreply.github.com>
2023-04-16 07:36:14 -0300

    Update exercicio-tabuada-for.js

--- exercicios/exercicio-tabuada-for.js
+++ exercicios/exercicio-tabuada-for.js
@@ -6,7 +6,3 @@ for (let i = 1; i <= 10; i++) {
   resultado = numero * i;
   console.log(numero + ' * ' + i + ' = ' + resultado);
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 1ff366fb80a36c4d41cdc240585fd403e4ad4155
